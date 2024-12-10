00a3ef76af2f4695a1dba4c185e1dc2aa571049d
k3nn3th255 <123401417+k3nn3th255@users.noreply.github.com>
2024-12-09 23:38:17 -0600

    se creo apartado reservas

--- backend/dbConection.php
+++ backend/dbConection.php
@@ -10,7 +10,7 @@ try {
     // Crear la conexión PDO con los datos proporcionados
     $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //logDebug("DB: Conexion Exitosa");
+    logDebug("DB: Conexion Exitosa");
 } catch (PDOException $e) {
     logError($e->getMessage());
     die("Error de conexion: " . $e->getMessage());
